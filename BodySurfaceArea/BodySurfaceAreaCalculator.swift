2bd8ba048bbfabefa7a63a890ae94edf522c4858
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-11-12 10:08:49 -0300

    Created calculator class

---
+++ BodySurfaceArea/BodySurfaceAreaCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  BodySurfaceAreaCalculator.swift
+//  BodySurfaceArea
+//
+//  Created by Luciano Sclovsky on 12/11/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import Foundation
